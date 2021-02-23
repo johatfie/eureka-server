f45fdfe9ea88c57148434f20257ee32ba141591a
Jon Hatfield <johatfie@gmail.com>
2021-02-23 03:28:13 -0500

    Add src folder

---
+++ src/main/java/com/widgets_are_us/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.widgets_are_us.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
